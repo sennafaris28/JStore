97d6243385105c9605c9f2f74eebdf285048ebb3
Senna Faris <senna.faris@ui.ac.id>
2019-02-25 13:29:35 +0700

    modul1_reupload

---
+++ JStore.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class JStore here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JStore
+{
+    public void main(String args[]) {
+
+    }
+
+    public JStore()
+    {
+
+    }
+}
